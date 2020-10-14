508224999278a53d31a4695f14d1b48e09145580
VietTQ1411 <46323176+VietTQ1411@users.noreply.github.com>
2020-10-14 23:48:09 +0700

    vn

--- Nodejs/databases/database.js
+++ Nodejs/databases/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 const mysql2 = require('mysql2')
 const { Sequelize } = require('sequelize')
-const  {
-    MAXMUM_FILE_SIZE, 
+const {
+    MAXMUM_FILE_SIZE,
     PORT,
     HOSTNAME,
     DB_PORT,
@@ -14,22 +14,21 @@ const  {
 const sequelize = new Sequelize(
     DB_NAME,
     DB_USERNAME,
-    DB_PASSWORD,
-    {
-      dialect: 'mysql',
-      dialectModule: mysql2, 
-      host: HOSTNAME,
-      port: DB_PORT
+    DB_PASSWORD, {
+        dialect: 'mssql',
+        dialectModule: mysql2,
+        host: HOSTNAME,
+        port: DB_PORT
     }
-  )
+)
 
 sequelize.authenticate()
-  .then(() => {
-      console.log('Connection Sequelize successfully.');
-  }).catch(err => {
-      console.error('Sequelize connect database failed:', err);
-  })
-  
+    .then(() => {
+        console.log('Connection Sequelize successfully.');
+    }).catch(err => {
+        console.error('Sequelize connect database failed:', err);
+    })
+
 module.exports = {
     sequelize
 }
--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # Android-Assignment
  Assignment môn Android
  
- npm install --save expess
  npm install mysql
- npm install sequelize
- npm install expess-fileupload
- npm install --save nodemon
- npm install bcrypt
+
  vntaking.com hocj android
